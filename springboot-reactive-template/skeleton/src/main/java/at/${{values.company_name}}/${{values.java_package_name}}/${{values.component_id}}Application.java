3e40e30615f620a86dd0ac3175c690d5349dc5c1
avollmaier <alois.vollm@gmail.com>
2023-09-03 15:47:13 +0200

    some config changes

--- springboot-reactive-template/skeleton/src/main/java/at/${{values.company_name}}/${{values.java_package_name}}/${{values.component_id}}Application.java
+++ springboot-reactive-template/skeleton/src/main/java/at/${{values.company_name}}/${{values.java_package_name}}/${{values.component_id}}Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ${{values.application_name}}Application {
+public class ${{values.component_id}}Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(${{ parameters.component_id }}Application.class, args);
+    SpringApplication.run(${{values.component_id}}Application.class, args);
   }
 }
